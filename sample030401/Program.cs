a558edd0632b1b5b1c08abad3a59200a9eebe480
lightcheap <lightcehap88@yahoo.co.jp>
2025-03-10 22:41:19 +0900

    next commit

--- sample030401/Program.cs
+++ sample030401/Program.cs
@@ -10,3 +10,29 @@ Console.WriteLine($"データ型 charは、{singleLetter} のような単一の�
 // 一つの文字列内に複数の変数や式を埋め込むこともできます。
 // {} で囲まれた部分には、変数や式の値が挿入されます。
 Console.WriteLine($"今日は {DateTime.Now.ToString("yyyy年MM月dd日")} です。現在の時刻は {DateTime.Now.ToString("HH:mm:ss")} です。");
+
+// C# でif文を書く
+// C#では "==="は使用しない。"=="で型の比較を行う。
+string name2 = "山田太郎";
+if (name2 == "山田太郎")
+{
+    Console.WriteLine("あなたの名前は山田太郎ですね。");
+}
+else
+{
+    Console.WriteLine("あなたの名前は山田太郎ではありません。");
+}
+
+string age = "20";
+if (age == "20")
+{
+    Console.WriteLine("あなたの年齢は20歳ですね。");
+}
+else
+{
+    Console.WriteLine("あなたの年齢は20歳ではありません。");
+}
+
+if (true) {
+
+}
